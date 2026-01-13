041a94709faa673a4037649b0bfe0b86175d731c
Dukyoon Im <DUKY8N@gmail.com>
2026-01-13 18:52:48 +0900

    feat: introduce PageLayout and Head components

---
+++ src/consts.ts
@@ -0,0 +1,32 @@
+import type { Site, Metadata, Socials } from '@/types'
+
+export const SITE: Site = {
+  NAME: 'Duky8n',
+  EMAIL: 'DUKY8N@gmail.com',
+}
+
+export const HOME: Metadata = {
+  TITLE: 'Home',
+  DESCRIPTION: `임덕윤(${SITE.NAME})의 홈페이지.`,
+}
+
+export const BLOG: Metadata = {
+  TITLE: 'Blog',
+  DESCRIPTION: '',
+}
+
+export const PROJECTS: Metadata = {
+  TITLE: 'Projects',
+  DESCRIPTION: '',
+}
+
+export const SOCIALS: Socials = [
+  {
+    NAME: 'x',
+    HREF: 'https://x.com/duky8n',
+  },
+  {
+    NAME: 'github',
+    HREF: 'https://github.com/DUKY8N',
+  },
+]
