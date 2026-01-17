06e239b8d88a3056979bad0af67acbb10ae7a5d3
Dukyoon Im <DUKY8N@gmail.com>
2026-01-17 18:53:11 +0900

    feat: update site configuration to include project and post limits

--- src/consts.ts
+++ src/consts.ts
@@ -3,6 +3,8 @@ import type { Site, Metadata, Socials } from '@/types'
 export const SITE: Site = {
   NAME: 'Duky8n',
   EMAIL: 'DUKY8N@gmail.com',
+  NUM_PROJECTS_ON_HOMEPAGE: 3,
+  NUM_POSTS_ON_HOMEPAGE: 3,
 }
 
 export const HOME: Metadata = {
--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,8 @@
 export type Site = {
   NAME: string
   EMAIL: string
+  NUM_PROJECTS_ON_HOMEPAGE: number
+  NUM_POSTS_ON_HOMEPAGE: number
 }
 
 export type Metadata = {
